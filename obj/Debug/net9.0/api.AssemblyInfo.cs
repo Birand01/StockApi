6afe19d9e66e01de0594ff0a4c89e18952f5424c
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-09 12:46:53 +0300

    Database connection is implemented

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d12a552154b502b59918f71d054412f8e3d09d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34e84ddd93b9d2263671120039264943765c33f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
