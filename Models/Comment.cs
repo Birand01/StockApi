b3d12a552154b502b59918f71d054412f8e3d09d
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-09 12:15:49 +0300

    Comment class is created

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -8,17 +8,11 @@ namespace api.Models
 {
     public class Comment
     {
-        public int Id { get; set; }
-        public string Symbol { get; set; }=string.Empty;
-        public string CompanyName{get;set;}=string.Empty;
-
-        [Column(TypeName ="decimal(18,2)")]
-        public decimal Purchase { get; set; } 
-         [Column(TypeName ="decimal(18,2)")]
-        public decimal LastDiv { get; set; } 
-        public string Industry { get; set; }=string.Empty;
-        public long MarketCap { get; set; }
-
-        public List<Comment> Comments{get;set;}=new List<Comment>();
+       public int Id { get; set; }
+       public string Title { get; set; }=string.Empty;
+       public string Content { get; set; }=string.Empty;
+       public DateTime CreatedOn { get; set; }=DateTime.Now;
+       public int? StockId { get; set; }
+       public Stock? Stock { get; set; }
     }
 }
