65bb6d1a6d4f736a098d16ab78438e290d78bc1e
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-09 14:22:49 +0300

    StockController is implemented

---
+++ Controller/StockController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controller
+{
+    public class StockController:ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public StockController(ApplicationDbContext context)
+        {
+            _context=context;
+        }
+        [HttpGet]
+        public IActionResult GetAllStock()
+        {
+            var stocks=_context.Stocks.ToList();
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStockById([FromRoute] int id)
+        {
+            var stock=_context.Stocks.Find(id);
+            if(stock is null)
+            {
+                return NotFound();
+            }
+            return Ok(stock);
+
+        }
+    }
+}
