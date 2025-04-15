c7788d46325407ef04ef657b1ed991d53f94183e
Birand Kılınç <pacmaidanapac@gmail.com>
2025-04-15 20:24:31 +0300

    Sorting is implemented to symbol and company name of stocks

--- obj/Debug/net9.0/api.AssemblyInfo.cs
+++ obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+445676d737b7f97868b2b89540067a06cb3208be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e229d9a51edd47fcbcd8efb999ead8c3de53bd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
